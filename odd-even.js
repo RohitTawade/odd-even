eabc0c577b7b67468c60fd9b39736658ac380294
Rohit Santram Tawade <115890889+RohitTawade@users.noreply.github.com>
2023-07-30 10:29:55 +0530

    odd-even.js

---
+++ odd-even.js
@@ -0,0 +1,15 @@
+const numbers = [1, 2, 3, 4, 5];
+
+const result = numbers.reduce((acc, num) => {
+  if (num % 2 === 0) {
+    acc.evens.push(num);
+  } else {
+    acc.odds.push(num);
+  }
+  return acc;
+}, { evens: [], odds: [] }); // Fix: Initialize the accumulator as an object with empty arrays
+
+console.log(result);
+
+// Output:
+{ evens: [2, 4], odds: [1, 3, 5] }
